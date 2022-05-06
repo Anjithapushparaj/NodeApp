02d983320f1cd88a852f686ab519df2b3f65f8fa
Anjithapushparaj <anjitha.pushparaj@gmail.com>
2022-05-06 12:40:43 +0530

    Added project

---
+++ src/schemas/restaurents.ts
@@ -0,0 +1,38 @@
+import { Schema, model } from "mongoose";
+
+const RestaurentSchema: Schema = new Schema({
+    restaurentId : { 
+        type: String, 
+        required: true
+    },
+    restaurentName : { 
+        type: String, 
+        required: true
+    } ,
+    location: { 
+        type: String, 
+        required: true
+    },
+    foodOptions : { 
+        type: [String], 
+        required: true
+    },
+    rating : { 
+        type: Number, 
+        required: true
+    },
+    isOpen : { 
+        type: Boolean, 
+        required: true
+    },
+    isVeg: { 
+        type: Boolean, 
+        required: true
+    },
+    diningOnly : { 
+        type: Boolean, 
+        required: true
+    }
+})
+
+export default model ("Restaurents", RestaurentSchema)
